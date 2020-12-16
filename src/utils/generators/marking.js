d899dac0b142865d76dff7c8452eae8218f4d6bf
tshkuro <tshkuro@bostonsd.ru>
2020-12-16 11:06:49 +0300

    Fix marking

--- src/utils/generators/marking.js
+++ src/utils/generators/marking.js
@@ -1,87 +1,79 @@
-function marking(map){
+
+function addNewRuleToMatcher(matcher, from, to) {
+	if (matcher[from] && matcher[from] != to) {
+		const newFrom = Math.max(to, matcher[from]);
+		const newTo = Math.min(to, matcher[from]);
+		this.addNewRuleToMatcher(matcher, newFrom, newTo);
+	}
+	matcher[from] = to;
+}
+
+function marking(map) {
 	let label = 1;
 	let fl = false; //взводим true, когда фон пойдет во второй раз (или сразу наткнемся на объект)
 	let result = [];
 	let matcher = []; // для переходов-конфликтов
-		
+
 	let fields = [];
-		
-		
-		//Разметка
-	for (let i = 0; i < map.length; ++i){ 
+
+
+	//Разметка
+	for (let i = 0; i < map.length; ++i) {
 		result[i] = [];
-    for (let j = 0; j < map[0].length; ++j){
-			switch(map[i][j]){
+		for (let j = 0; j < map[0].length; ++j) {
+			switch (map[i][j]) {
 				case 0:
-					if(fl){
+					if (fl) {
 						++label;
 						fl = false;
 					}
 					result[i][j] = 0;
-				break;
-					
+					break;
+
 				case 1:
 					fl = true;
-					if(!i)
+					if (!i)
 						result[i][j] = label;
 					else {
 						//проверяем номера соседей (4-х связные соседи)
-						const top = result[i-1][j];
-						const left = !j ? 0 : result[i][j-1];
-						if(top == left){
-							result[i][j] = top ? top : label; 
+						const top = result[i - 1][j];
+						const left = !j ? 0 : result[i][j - 1];
+						if (top == left) {
+							result[i][j] = top ? top : label;
 						}
-						else if(!top || !left){
+						else if (!top || !left) {
 							result[i][j] = Math.max(top, left);
 						} else {
 							let max = Math.max(top, left);
 							let min = Math.min(top, left);
-							result[i][j] = max; //???
-							let temp = matcher[max]; //для вложенных областей
-							matcher[max] = min;  //??
-								
-								
-								/*let cnt = 0;
-								while(temp && temp != min){
-									let temp2 = matcher[temp];
-									max = Math.max(min, temp);
-									min = Math.min(min, temp);
-									matcher[max] = min;
-									cnt++;
-									console.log("Nested " + cnt + ": " + max + " -> " + min)
-									temp = temp2;
-								}*/
-								//if(matcher[max] && matcher[max] != min){
-									//высокая вложенность
-								//	matcher[Math.max(min, matcher[max])] = Math.min(min, matcher[max]);
-								//} 
-								
+							result[i][j] = max;
+							addNewRuleToMatcher(matcher, max, min);
 						}
 					}
-				break;
+					break;
 			}
 		}
 		++label;
 	}
-		
+
 	//console.log(matcher)
-		
-		//Переразметка с учетом переходов (и сразу запихивание в список??)
-	for (let i = 0; i < map.length; ++i){ 
-		for (let j = 0; j < map[0].length; ++j){
+
+	//Переразметка с учетом переходов (и сразу запихивание в список??)
+	for (let i = 0; i < map.length; ++i) {
+		for (let j = 0; j < map[0].length; ++j) {
 			const item = result[i][j];
-			if(item){
+			if (item) {
 				//переразметка
-				if(matcher[item]){
+				if (matcher[item]) {
 					let m = matcher[item]
-					while(matcher[m])
+					while (matcher[m])
 						m = matcher[m];
 					result[i][j] = m;
 				}
-					//учет областей
+				//учет областей
 				let field = fields[result[i][j]];
-				if(!field){
-					fields[result[i][j]] = {left: i, right: i, top: j, bottom: j, pixels: 1 };
+				if (!field) {
+					fields[result[i][j]] = { left: i, right: i, top: j, bottom: j, pixels: 1 };
 				} else {
 					++field.pixels;
 					field.left = Math.min(field.left, i);
@@ -93,10 +85,10 @@ function marking(map){
 			}
 		}
 	}
-			
+
 	//console.log(result)
 	//console.log(fields);
 	return fields.length;
 }
-	
-	export { marking }
+
+export { marking }
