b0b6f2a9032aee34e060c0501469f073a8a21907
ChukhinAI <55681756+ChukhinAI@users.noreply.github.com>
2020-12-30 11:07:55 +0300

    Add files via upload

--- src/ai/steerings/pursuit.js
+++ src/ai/steerings/pursuit.js
@@ -6,6 +6,7 @@ export default class Pursuit extends Steering {
 
     constructor (owner, target, force = 1, ownerSpeed= 80, targetSpeed = 100) {
         super(owner, [target], force);
+
         this.ownerSpeed = ownerSpeed;
         this.targetSpeed = targetSpeed
     }
@@ -14,7 +15,6 @@ export default class Pursuit extends Steering {
         const desiredVelocity = new Vector2(target.x - owner.x, target.y-owner.y)
         .normalize().scale(maxSpeed);
         const prevVelocity = new Vector2(owner.body.x-owner.body.prev.x, owner.body.y-owner.body.prev.y);
-        //console.log(owner.evader.body.prev.x,desiredVelocity)
         return desiredVelocity.subtract(prevVelocity);
     }
 
@@ -24,9 +24,10 @@ export default class Pursuit extends Steering {
         // console.log(this.objects[0].Steering.objects[0])
         // ev = 5;
         const target = this.objects[0];
+
         let pursuitMen;
         if (target instanceof Npc)
-            pursuitMen = this.objects[0].Steering.objects[0];
+            pursuitMen = this.objects[0].steering.objects[0];
         else    
             pursuitMen = this.owner.evader;
 
@@ -34,22 +35,23 @@ export default class Pursuit extends Steering {
         const targetPos = new Vector2(target.x, target.y);
         const targetDirection = target.body.velocity;
         const toTarget = new Vector2(pursuitMen.x - target.x, pursuitMen.y - target.y);
+
         const relativeHeading = searcherDirection.dot(targetDirection);
-        //console.log(target.x, pursuitMen.x,target.y, pursuitMen.y)
-        
+
+        if (Math.abs(target.x - pursuitMen.x) < 40 && Math.abs(target.y - pursuitMen.y) < 40)
+            return  new Vector2(0, 0);
+
+
         if (Math.abs(target.x - pursuitMen.x) < 40 && Math.abs(target.y - pursuitMen.y) < 40)
             return  new Vector2(0, 0);
 
         if (toTarget.dot(targetDirection) < 0 || relativeHeading > -0.95)
             return Pursuit.seek(pursuitMen, targetPos, this.ownerSpeed);
 
-        
-            
-              
-        
         const lookAheadTime = toTarget.length / (this.ownerSpeed + this.targetSpeed)
         
         return Pursuit.seek(pursuitMen, 
+
             targetPos.add(target.body.velocity.clone().scale(lookAheadTime)), 
             this.ownerSpeed);
     }
--- src/characters/character_factory.js
+++ src/characters/character_factory.js
@@ -1,25 +1,30 @@
 import {StateTableRow, StateTable} from '../ai/behaviour/state';
 import Slime from "./slime";
 import Player from "./player";
-
-import NPC from "../characters/npc";
 import cyberpunkConfigJson from "../../assets/animations/cyberpunk.json";
 import slimeConfigJson from "../../assets/animations/slime.json";
+import mineConfigJson from '../../assets/animations/mine.json';
 import AnimationLoader from "../utils/animation-loader";
-import npc from "./npc";
-import Wandering from "../ai/steerings/wandering";
-import Arrival from "../ai/steerings/arrival";
 
-
+import Mine from "./mine";
+import SmartSlime from './minerScene/smartSlime';
+import NPC from "../characters/npc";
 
 export default class CharacterFactory {
+
+
+
     constructor(scene) {
         this.scene = scene;
+
         this.cyberSpritesheets =  ['aurora', 'blue', 'yellow', 'green', 'punk'];
         this.slimeSpriteSheet = 'slime';
+
+        this.mineSpriteSheet = 'mine';
         const slimeStateTable = new StateTable(this);
         slimeStateTable.addState(new StateTableRow('searching', this.foundTarget, 'jumping'));
         slimeStateTable.addState(new StateTableRow('jumping', this.lostTarget, 'searching'));
+
         let animationLibrary =  new Map();
         this.cyberSpritesheets.forEach(
             function (element) {
@@ -29,8 +34,13 @@ export default class CharacterFactory {
                     element).createAnimations());
             }
         );
-        animationLibrary.set(this.slimeSpriteSheet,
-                new AnimationLoader(scene, this.slimeSpriteSheet, slimeConfigJson, this.slimeSpriteSheet).createAnimations());
+        animationLibrary
+            .set(this.mineSpriteSheet, new AnimationLoader(scene, this.mineSpriteSheet, mineConfigJson, this.mineSpriteSheet)
+            .createAnimations());
+        animationLibrary
+            .set(this.slimeSpriteSheet,
+            new AnimationLoader(scene, this.slimeSpriteSheet, slimeConfigJson, this.slimeSpriteSheet)
+                .createAnimations());
         this.animationLibrary = animationLibrary;
     }
 
@@ -40,55 +50,49 @@ export default class CharacterFactory {
             case 'punk':
             case 'aurora':
             case 'blue':
-            case 'punk':
-                if (params.player)
-                    return this.buildPlayerCharacter(spriteSheetName,x,y);
-
             case 'yellow':
-            case 'green':
-              if (params.player)
-                return this.buildPlayerCharacter(spriteSheetName, x, y);
-              else{
-                return this.buildNPCCharacter(spriteSheetName, x, y, params);
-              }
-
+                if (params.player)
+                    return this.buildPlayerCharacter(spriteSheetName, x, y, params);
+                else {
+                        return this.buildNPCCharacter(spriteSheetName, x, y, params);
+                }
             case "slime":
-              return this.buildSlime(x, y, params);
+                return this.buildSlime(x, y, params);
+            case "mine":
+                return this.buildMine(x, y, params);
         }
     }
-	
-		buildNPCCharacter(spriteSheetName, x, y, params) {
-        let character = new NPC(this.scene, x, y, spriteSheetName, 2);
-				if(params.steering){
-					character.steering = this.getSteerings(params, character, []);
-				}
+
+
+    buildNPCCharacter(spriteSheetName, x, y, params) {
+        let character = new NPC(this.scene, x, y,
+            spriteSheetName, 2,
+            params.Steering);
+
         character.animationSets = this.animationLibrary.get(spriteSheetName);
         return character;
     }
 
-    buildNpcCharacter(animation,spriteSheetName, x, y,params){
-        let character = new npc(this.scene,x,y,spriteSheetName,2,params.Steering)
-        character.animationSets = this.animationLibrary.get(animation);
-        return character;
-    }
-    buildPlayerCharacter(spriteSheetName, x, y) {
-        let character = new Player(this.scene, x, y, spriteSheetName,2);
+    buildPlayerCharacter(spriteSheetName, x, y, params = {}) {
+        let character = new Player(this.scene, x, y, spriteSheetName, 2, params);
         character.maxSpeed = 100;
         character.setCollideWorldBounds(true);
         character.cursors = this.scene.input.keyboard.createCursorKeys();
         character.animationSets = this.animationLibrary.get('aurora');
         //todo: not here
-      character.footstepsMusic = this.scene.sound.add('footsteps', {
-          mute: false,
-          volume: 1,
-          rate: 1,
-          detune: 0,
-          seek: 0,
-          loop: true,
-          delay: 0
-      });
-      //todo uncomment at your won risk - these footsteps will get you insane
-      //character.footstepsMusic.play();
+        character.footstepsMusic = this.scene.sound.add('footsteps', {
+            mute: false,
+            volume: 1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: true,
+            delay: 0
+        });
+        //todo uncomment at your won risk - these footsteps will get you insane
+        // character.footstepsMusic.play();
+
+
         return character;
 
     }
@@ -100,34 +104,37 @@ export default class CharacterFactory {
     }
 
     buildSlime(x, y, params) {
-        const slimeType = params.slimeType || 1;
-				let slime = new Slime(this.scene, x, y, this.slimeSpriteSheet, 9 * slimeType);
-				if(params.steering)
-					slime.steering = this.getSteerings(params, slime);
-        slime.animations = this.animationLibrary.get(this.slimeSpriteSheet).get(this.slimeNumberToName(slimeType));
+
+        const slimeType = params.slimeType;
+        let slime;
+        if (params.useSteering) {
+            slime = new SmartSlime(this.scene, x, y, this.slimeSpriteSheet, 9*slimeType);
+        } else {
+            slime = new Slime(this.scene, x, y, this.slimeSpriteSheet, 9 * slimeType);
+        }
+
+        slime.animations = this.animationLibrary
+                .get(this.slimeSpriteSheet)
+                .get(this.slimeNumberToName(slimeType));
         slime.setCollideWorldBounds(true);
         slime.speed = 40;
         return slime;
     }
-		
-		getSteerings(params, owner){
-			switch(params.steering){
-				case "wandering": 
-					return new Wandering(owner, params.target);
-				case "arrival":
-					return new Arrival(owner, params.target);
-				default:
-					return null;
-			}
-		}
-		
-    slimeNumberToName(n){
-      switch (n) {
-        case 0: return 'Blue';
-        case 1: return 'Green';
-        case 2: return 'Orange';
-        case 3: return 'Pink';
-        case 4: return 'Violet';
-      }
+
+    slimeNumberToName(n)
+    {
+        switch (n) {
+            case 0: return 'Blue';
+            case 1: return 'Green';
+            case 2: return 'Orange';
+            case 3: return 'Pink';
+            case 4: return 'Violet';
+        }
+
+    }
+    
+    buildMine(x, y, params) {
+        let mine = new Mine(this.scene, x, y, this.mineSpriteSheet, 0);
+        mine.animations = this.animationLibrary.get(this.mineSpriteSheet).get("Mine");
     }
 }
